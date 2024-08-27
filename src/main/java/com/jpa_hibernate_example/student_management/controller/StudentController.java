d093efa9e67cca32f8566b97a7a88e024dfaaeec
Chandan K M <chandan.km@tecnotree.com>
2024-08-27 20:42:03 +0530

    JPA and Hibernate demo

--- src/main/java/com/jpa_hibernate_example/student_management/controller/StudentController.java
+++ src/main/java/com/jpa_hibernate_example/student_management/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.jpa_hibernate_example.student_management.controller;
 import com.jpa_hibernate_example.student_management.model.Student;
 import com.jpa_hibernate_example.student_management.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -18,9 +17,21 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/save")
-    public String saveStudent(@RequestBody Student student){
+    public String saveStudent(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "Student saves successfully";
+    }
+
+    @GetMapping("/find/{studentid}")
+    public Student getStudentById(@PathVariable("studentid") int studentId) {
+        Student student = studentService.getStudentById(studentId);
+        return student;
+    }
+
+    @GetMapping("/findall")
+    public List<Student> getAllStudents(){
+        List<Student> studentList = studentService.getAllStudents();
+        return studentList;
     }
 
 }
--- src/main/java/com/jpa_hibernate_example/student_management/service/StudentService.java
+++ src/main/java/com/jpa_hibernate_example/student_management/service/StudentService.java
@@ -5,6 +5,9 @@ import com.jpa_hibernate_example.student_management.repository.StudentRepository
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service // it contains business logic
 public class StudentService {
 
@@ -13,5 +16,15 @@ public class StudentService {
 
     public void saveStudent(Student student){
         studentRepository.save(student);
+    }
+
+    public Student getStudentById(int studentId){
+        Optional<Student> studentFromDb = studentRepository.findById(studentId);
+        return studentFromDb.get();
+    }
+
+    public List<Student> getAllStudents(){
+        List<Student> studentList = studentRepository.findAll();
+        return studentList;
     }
 }
